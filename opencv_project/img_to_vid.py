93d626cbcc34fdf62422d7bf66385dcff447b5ea
AndreW13-sp <panchalswaraj13@gmail.com>
2022-10-26 18:23:08 +0530

    Images to video

--- opencv_project/img_to_vid.py
+++ opencv_project/img_to_vid.py
@@ -1,35 +1,46 @@
 import cv2
+import random
 import os
+import json
 
-path = '/Users/swarajpanchal/Desktop/Img to vid/opencv_project/imgs/'
-output_path = '/Users/swarajpanchal/Desktop/Img to vid/opencv_project/vid/'
-output_video_name = 'sunset1.mp4'
+path = 'opencv_project/imgs/'
+output_path = 'opencv_project/vid/'
+output_video_name = 'sunset.mp4'
 output_video_full_path = output_path + output_video_name
 
-pre_imgs = os.listdir(path)
+pre_imgs = os.listdir(path) #for making an array of images in memory
 # print(pre_imgs)
-img = []
+img = [] #creating empty array of images
 
-for i in pre_imgs:
+for i in pre_imgs:  #loop for pre_images
     i = path+i 
     # print(i) 
     img.append(i)
 
 # print(img)
 
-cv2_fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-frame = cv2.imread(img[0])
-print(frame)
-size = list(frame.shape)
-print(frame.shape)
-del size[2]
-#size.reverse()
+cv2_fourcc = cv2.VideoWriter_fourcc(*'mp4v') #in open cv2 thier is videowiter function for making video
+frame = cv2.imread(img[0]) #frame of images (border of image)
+size = list(frame.shape) #width and height of image array
+del size[2] #deleting 3rd element of size
+#size.reverse() 
 # print(size)
 
-video = cv2.VideoWriter(output_video_full_path, cv2_fourcc, 1, size) #output: video name, fourcc, fps, size
-counter = 0
-for i in img:
-    video.write(cv2.imread(i))
+video = cv2.VideoWriter(output_video_full_path, cv2_fourcc, 1, size) #output: video name, fourcc, fps, size (videowriter is class of cv2 and creating object and passing the required arrguments)
+counter = 0 
+imgsizes = [2,0,3,4,1]# frame time in seconds of individual frames
+#print(img)
+data= dict(zip(img, imgsizes))#creating a dictionary of images and its frame type
+#looping through data dictionary to get a corresponding img and its frame type
+for sp ,img_size in data.items(): #inserting frames into the video object 
     print('frame', counter+1, 'of', len(img))
     counter+=1
-video.release()
+    with open(sp) as f:
+        #checking for the img file extention (.jpeg)
+        if f.name.endswith('.jpeg'):
+    #         for j in range(random.randint(1,9)):
+            for i in range (0,img_size):
+                video.write(cv2.imread(sp))
+#print(data)
+
+video.release() #relase video into the given path
